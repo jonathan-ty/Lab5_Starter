77b8ab23d339388c19b38d86e7de1c836e1a23dd
Jonathan Ty <jonathanty42@gmail.com>
2025-05-04 22:51:26 -0700

    Refactor expose.js: modularize event handling and improve code readability

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,57 +2,65 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-// Implementation
-// You may notice an “init()” function in your JS files - this is an initialize function. It isn’t called until the whole DOM has been parsed, so it’s a good idea to have your functions / element queries stem from this init() function, otherwise you run the risk of trying to query an element that hasn’t loaded yet.
+// Global Variables
+let CURRENT_HORN = "";
+let CURRENT_VOLUME = parseInt(volume.value) / 100;
+const jsConfetti = new JSConfetti();
 
-// Requirements:
-// When you select a horn from the drop down menu, the following should occur:
-// The correct image should display
-// The correct audio sound file should be set
-// When you change the volume on the slider, the following should occur:
-// At zero volume, the mute icon (level 0) should be displayed
-// From 1 to < 33 volume the volume level one svg should be displayed
-// From 33 to < 67 volume the volume level two svg should be displayed
-// From 67 and up the volume level three svg should be displayed
-// The corresponding volume should be set for the audio element (note: this element’s volume is not out of 100)
-// Hint: the volume slider's range is from 0 - 100, but the audio element has a different propertyLinks to an external site..
-// When you click the “Play Sound” button the following should occur:
-// The corresponding sound for the horn selected should play out loud at the specified volume
-// If the Party Horn is selected, confetti should shoot out when the play button is clicked, as shown in the video
-// A library has been included for you to accomplish this, more on how to use it here https://github.com/loonywizard/js-confettiLinks to an external site.
-// Do not run the installation steps, or include the import statement as we have already installed and imported the confetti library for you
-// Once you are done, please publish your explose.html on GitHub pages.
 function init() {
   const horn = document.getElementById("horn-select");
   const button = document.querySelector("button");
   const img = document.querySelector("img");
   const audio = document.querySelector("audio");
   const volume = document.getElementById("volume");
+  const volume_level = document.querySelector("#volume-controls img");
 
-  let CURRENT_HORN = "";
-  let CURRENT_VOLUME = volume.value;
-
-  horn.addEventListener("change", (event) => {
-    CURRENT_HORN = event.target.value;
-    if (CURRENT_HORN === "select") {
-      img.src = `assets/images/no-image.png`;
-    } else {
-      img.src = `assets/images/${CURRENT_HORN}.svg`;
-    }
+  horn.addEventListener("change", () => {
+    setImage(horn, img);
   });
 
-  volume.addEventListener("change", (event) => {
-    CURRENT_VOLUME = parseInt(event.target.value)/100;
+  volume.addEventListener("input", () => {
+    setVolume(volume_level, audio);
   });
 
-  button.addEventListener("click", (event) => {
-    if (CURRENT_HORN === "select") {
-      audio.src = "";
-    } else {
-      audio.src = `assets/audio/${CURRENT_HORN}.mp3`;
-      console.log(CURRENT_HORN);
-    }
-    audio.volume = CURRENT_VOLUME;
-    audio.play();
+  button.addEventListener("click", () => {
+    setSound(audio);
   });
+}
+
+function setImage(horn, img) {
+  CURRENT_HORN = horn.value;
+  if (CURRENT_HORN === "select") {
+    img.src = `assets/images/no-image.png`;
+  } else {
+    img.src = `assets/images/${CURRENT_HORN}.svg`;
+    img.alt = `${CURRENT_HORN}`;
+  }
+}
+
+function setVolume(volume_level, audio) {
+  CURRENT_VOLUME = parseInt(volume.value);
+  if (CURRENT_VOLUME === 0) {
+    volume_level.src = `assets/icons/volume-level-0.svg`;
+  } else if (CURRENT_VOLUME < 33) {
+    volume_level.src = `assets/icons/volume-level-1.svg`;
+  } else if (CURRENT_VOLUME < 67) {
+    volume_level.src = `assets/icons/volume-level-2.svg`;
+  } else {
+    volume_level.src = `assets/icons/volume-level-3.svg`;
+  }
+  CURRENT_VOLUME /= 100;
+  audio.volume = CURRENT_VOLUME;
+}
+
+function setSound(audio) {
+  if (CURRENT_HORN === "select") {
+    audio.src = "";
+  } else {
+    audio.src = `assets/audio/${CURRENT_HORN}.mp3`;
+  }
+  if (CURRENT_HORN === "party-horn") {
+    jsConfetti.addConfetti();
+  }
+  audio.play();
 }
